48338b4e202a3284bbee33ae3fed11a796778863
deepika621 <18311a12l2@sreenidhi.edu.in>
2023-07-25 21:49:08 +0530

    Edit Existing Contact

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -59,6 +59,9 @@ class Program
         addressBook.AddContact(myContact);
 
         addressBook.PrintContacts();
+
+        // 2.Ability to add new Contact to address book
+
         // Create a new dictionary to store contacts
         Dictionary<string, string> contacts = new Dictionary<string, string>();
 
@@ -82,9 +85,42 @@ class Program
             Console.WriteLine("{0}: {1}", contact.Key, contact.Value);
         }
 
-        // Wait for the user to press a key before exiting
-       // Console.WriteLine("Press any key to exit.");
-        Console.ReadKey();
+            // 3.Ability to edit exixting contact person using their name
+
+            // Display the initial address book
+            Console.WriteLine("Initial Address Book:");
+            foreach (KeyValuePair<string, string> contact in contacts)
+            {
+                Console.WriteLine("{0}: {1}", contact.Key, contact.Value);
+            }
+
+            // Ask the user which contact to edit
+            Console.Write("Enter the name of the contact to edit: ");
+            name = Console.ReadLine();
+
+            // Check if the contact exists
+            if (contacts.ContainsKey(name))
+            {
+                // Ask the user to enter the new phone number
+                Console.Write("Enter the new phone number: ");
+                phone = Console.ReadLine();
+
+                // Update the contact's phone number
+                contacts[name] = phone;
+
+                // Display the updated address book
+                Console.WriteLine("Updated Address Book:");
+                foreach (KeyValuePair<string, string> contact in contacts)
+                {
+                    Console.WriteLine("{0}: {1}", contact.Key, contact.Value);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Contact not found.");
+            }
+
+            Console.ReadKey();
     }
 }
 }
